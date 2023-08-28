c72de4bb1d6ed10338fca3551c2ae416af236d84
Vasily Styagov <styagov.dev@gmail.com>
2023-08-28 21:28:08 +0100

    Add cases to widgetbook

---
+++ storybook/lib/text.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:ui_kit/ui_kit.dart';
+import 'package:ui_kit/widgets/counter_text.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+final textsFolder = WidgetbookFolder(
+  name: 'Texts',
+  children: [
+    appBarTitleComponent,
+    counterTextComponent,
+  ],
+);
+
+final appBarTitleComponent = WidgetbookComponent(
+  name: 'AppBar Title',
+  useCases: [
+    WidgetbookUseCase(
+      name: 'Default',
+      builder: (context) => Center(
+        child: AppBarTitle(
+          text: context.knobs.string(label: 'Text', initialValue: 'Title'),
+        ),
+      ),
+    ),
+  ],
+);
+
+final counterTextComponent = WidgetbookComponent(
+  name: 'Counter Text',
+  useCases: [
+    WidgetbookUseCase(
+      name: 'Default',
+      builder: (context) => Center(
+        child: CounterText(
+          value: context.knobs.double.input(label: 'Value', initialValue: 0).toInt(),
+        ),
+      ),
+    ),
+  ],
+);
